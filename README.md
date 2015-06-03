e977034918ba459ee19651a74b838ac8f285d8e1
Elias A. G.-C <eliasagc@gmail.com>
2015-06-03 18:26:45 +0200

    already finished. In one commit. How cool is that?

---
+++ README.md
@@ -0,0 +1,18 @@
+# You can touch
+
+Tickle is a simple, robust and readable javascript library for handling touch interaction with sanity. It's implemented in a way that allows you to read the code and understand what it does pretty easily, so if you want to do some oh-so-exotic finger contortion on your UI, you can just fire your editor and add it.
+
+If you skip all the noise I had to add at the top of `ticke.js` just to make sure you can use it with your current setup (whichever it may be), and then you skip some more and jump over all the functions with self-explanatory names like `clone` and `calculatePosition`, you'll end in a big Finite State Machine declaration. That's the meat. That's how this beauty works. The retarded browser events are abstracted as abstract *inputs* to the machine. The machine will change it's state according to the rules defined there, and you can see pretty well the full cycle of events that has to happen for a `@tap` to be detected or a `@pinch` to occur. All the nonsensical intermediate states are there just to rest while the user is trying to decide what to do. For example, you can detect a `touchstart` but that doesn't tell you anything. What's going to happen next? If the user raises her finger fast enough, then you have a `@tap`. If she moves it around, then you have a `@drag`. If she puts another finger down, and *then* moves them independently, we are getting a `@pinch` gesture. So, you see, it's not as easy as `touchstart`, `touchend`, `touchmove`. A gesture needs a *full sequence of events* to determine it's own nature. And that's what the state machine does: provide memory and understand sequences.
+
+## How peachy. Now, look: I'm a mindless zombie and just want to use it now. Plug-n-Play. Don't waste my brain cycles.
+
+Well, good news! You only have to follow this simple steps:
+
+* Download the library, include it in your HTML or whatever.
+* You see the `emit` method? Is around the third quarter of the file. Great. Don't lose it.
+* Implement whatever you need inside `emit`. Can be a event-emitter of sorts. Or a dispatcher. Or a callback. You know, sky is the limit. Be creative.
+* That's cool, right? Can you get more flexible than that? You get to implement *your own method*, the method *you always dreamed about*! And *for free*!
+
+## Wow, thank you. You made my day.
+
+And you made mine, dear user. I'll be right here if you need some touching again.
---
+++ tickle.js
@@ -0,0 +1,340 @@
+var FSM = require('../lib/fsm');
+
+var configConstants = {
+  dragThreshold: 10,
+  swipeThreshold: 30,
+  swipeTimeout: 10000,
+  swipeSpeedThreshold: 0.2,
+  clickEnabled: true,
+  dragEnabled: true,
+  swipeEnabled: true,
+  pinchEnabled: false,
+  timeout: 500
+};
+
+// Some Utils
+
+function assign() {
+  var args = Array.prototype.slice.call(arguments);
+  return args.reduce(function(acc, el) {
+    for (var k in el) if (el.hasOwnProperty(k)) acc[k] = el[k];
+    return acc;
+  }, {});
+}
+
+function bindAll(obj, ctx) {
+  return Object.keys(obj).reduce(
+    function(acc, k) { acc[k] = obj[k].bind(ctx); return acc; }, {});
+}
+
+// FSM library (embedded here for portability purposes)
+
+function FSM() {}
+
+FSM.prototype = assign(FSM.prototype, {
+  start: function() {
+    this.runState(this.initialState);
+    return this;
+  },
+  send: function(input) {
+    var args = Array.prototype.slice.call(arguments, 1),
+        currentState = this.getCurrentState(),
+        nextState = this.getNextState(currentState, input);
+    return nextState? this.runState(nextState, args) : void 0;
+  },
+  runState: function(nextState, args) {
+    // console.log(@~'#[FSM] ${this.getCurrentState()} -> ${nextState}');
+    this.changed = false;
+    if (!(args instanceof Array)) { args = [args]; }
+    this.onStateChange(this.currentState, nextState, args);
+    if (this.states[nextState]) {
+      this.states[nextState].apply(this, args || []);
+    }
+    if (!this.changed) {
+      this.changed = true;
+      this.setCurrentState(nextState);
+    }
+  },
+  onStateChange: function(state, nextState, args) {
+    // nop
+  },
+  getNextState: function(currentState, input) {
+    try {
+      return this.transitions[currentState][input];
+    } catch (e) {
+      return undefined;
+    }
+  },
+  getCurrentState: function() {
+    return this._currentState;
+  },
+  setCurrentState: function(state) {
+    this._currentState = state;
+  }
+});
+
+// Touch handling utilities
+
+function clone(touches) {
+  var copy = [{
+    screenX: touches[0].screenX,
+    screenY: touches[0].screenY
+  }];
+  if (touches[1]) {
+    copy.push({
+      screenX: touches[1].screenX,
+      screenY: touches[1].screenY
+    });
+  }
+  return copy;
+}
+
+function calculatePosition(reference, touches) {
+  var ref = calculateCenter(reference),
+      actual = calculateCenter(touches);
+  return {x: actual.x - ref.x, y: actual.y - ref.y};
+}
+
+function calculateZoom(reference, touches) {
+  var ref = calculateDistance(reference),
+      actual = calculateDistance(touches),
+      zoom = (actual - ref) / 100;
+  return {x: zoom, y: zoom};
+}
+
+function calculateDistance(touches) {
+  return Math.sqrt(
+    Math.pow(touches[0].screenX - touches[1].screenX, 2) +
+      Math.pow(touches[0].screenY - touches[1].screenY, 2)
+  );
+}
+
+function calculateCenter(touches) {
+  return {
+    x: (touches[0].screenX + touches[1].screenX) / 2,
+    y: (touches[0].screenY + touches[1].screenY) / 2
+  };
+}
+
+function calculateDelta(touch0, touch1) {
+  return {
+    x: touch1.screenX - touch0.screenX,
+    y: touch1.screenY - touch0.screenY
+  };
+}
+
+function calculateSpeed(distance, time) {
+  return distance / time;
+}
+
+function fireOriginalEvent(origE) {
+  var e = new CustomEvent('touchstart', origE);
+  e.touches = Array.prototype.slice(origE.touches);
+  origE.srcElement.dispatchEvent(e);
+  // e = new CustomEvent('click', origE);
+  // origE.srcElement.dispatchEvent(e);
+}
+
+function fireClickEvent(origE) {
+  var e = new CustomEvent('click', origE);
+  origE.srcElement.dispatchEvent(e);
+}
+
+// The State Machine
+
+var TouchDaemon = assign({}, FSM.prototype, {
+  // remove this method to reuse the library
+  emit: function(event, e, params) {
+      e = e? {srcElement: e.srcElement, target: e.currentTarget,
+              originalEvent: e} : {};
+    // HEY! USER! What do you want to do with the event?
+    // doWhateverYouNeed(event, e, params);
+  },
+  start: function() {
+    this.handlers = bindAll(this.handlers, this);
+    this.setListeners();
+    return FSM.prototype.start.call(this);
+  },
+  stop: function() {
+  },
+  setListeners: function() {
+    document.body.addEventListener('touchstart', this.handlers.onTouchstart, true);
+    document.body.addEventListener('touchend', this.handlers.onTouchend, true);
+    document.body.addEventListener('touchmove', this.handlers.onTouchmove, true);
+  },
+  removeListeners: function() {
+    document.body.removeEventListener('touchstart', this.handlers.onTouchstart, true);
+    document.body.removeEventListener('touchend', this.handlers.onTouchend, true);
+    document.body.removeEventListener('touchmove', this.handlers.onTouchmove, true);
+  },
+  initialState: '@waiting',
+  onStateChange: function() {
+    window.clearTimeout(this.timerId);
+    this.timerId = window.setTimeout(this.handlers.onTimeout, this.timeout);
+  },
+  handlers: {
+    onTouchstart: function(e) {
+      e.stopPropagation();
+      if (e.touches.length === 1) {
+        // calculate drag threshold
+        this._movedRef = clone(e.touches);
+        this._refDistance = 0;
+        this.send('FIRST_FINGER_DOWN', e);
+      } else if (e.touches.length === 2) {
+        this.send('SECOND_FINGER_DOWN', e);
+      }
+    },
+    onTouchend: function(e) {
+      if (e.changedTouches.length === 2) {
+        this.send('TOUCH_END_BOTH_FINGERS', e);
+      } else if (e.touches.length === 1) {
+        this.send('TOUCH_END_SECOND_FINGER', e);
+      } else {
+        this.send('TOUCH_END_FIRST_FINGER', e);
+      }
+    },
+    onTouchmove: function(e) {
+      if (this._refDistance > configConstants.dragThreshold) {
+        this.send('TOUCH_MOVE', e);
+      } else {
+        this._refDistance += calculateDistance([e.touches[0],
+                                                this._movedRef[0]]);
+      }
+    },
+    onTimeout: function() {
+      this.send('TIME_OUT');
+    },
+    idle: function() {}
+  },
+  states: {
+    '@waiting': function() {
+      this.reference = null;
+      this.lastState = null;
+      this.refEvent = null;
+    },
+    '@hold': function(e) {
+      this.emit('touch', e);
+      this.refEvent = e;
+    },
+    '@dragstart': function(e) {
+      this.reference = clone(e.touches);
+      this.lastState = {delta: {x: 0, y: 0}, speed: 0, stamp: Date.now(),
+                        touches: clone(e.touches)};
+      this.emit('dragstart', e);
+      this.runState('@drag', e);
+    },
+    '@drag': function(e) {
+      // TO THINK: Or should I emit the WHOLE distance, from this.reference?
+      var distance = calculateDistance([e.touches[0], this.lastState.touches[0]]),
+          now = Date.now(),
+          delta = calculateDelta(this.lastState.touches[0], e.touches[0]),
+          totalDelta = calculateDelta(this.reference[0],
+                                      this.lastState.touches[0]),
+          totalDistance = calculateDistance([this.reference[0],
+                                             this.lastState.touches[0]]),
+          speed = calculateSpeed(distance, now - this.lastState.stamp);
+      this.emit('drag', e, {delta: delta, totalDelta: totalDelta,
+                            speed: speed, totalDistance: totalDistance});
+      this.lastState = {delta: delta, distance: distance, speed: speed,
+                        stamp: now, touches: clone(e.touches)};
+    },
+    '@swipe': function(e) {
+      var totalDistance = calculateDistance([this.reference[0],
+                                             this.lastState.touches[0]]),
+          totalDelta = calculateDelta(this.reference[0],
+                                      this.lastState.touches[0]),
+          directionRatio = Math.abs(totalDelta.x / (totalDelta.y || 0.01));
+      // Possible conditions to try: total distance, last distance, speed
+      // TODO: remove this comment if the condition works well
+      if (this.lastState.speed > this.swipeSpeedThreshold) {
+        if (directionRatio > 2) {
+          this.emit(totalDelta.x > 0? 'swipe:right' : 'swipe:left', e,
+                    {distance: Math.abs(totalDelta.x),
+                     speed: this.lastState.speed});
+        } else if (directionRatio < 0.5) {
+          this.emit(totalDelta.y > 0? 'swipe:down' : 'swipe:up', e,
+                    {distance: Math.abs(totalDelta.y),
+                     speed: this.lastState.speed});
+        }
+        this.emit('swipe', e, {delta: totalDelta, distance: totalDistance,
+                               speed: this.lastState.speed});
+      }
+      this.runState('@dragend', e);
+    },
+    '@dragend': function(e) {
+      this.emit('dragend', e);
+      this.runState('@waiting');
+    },
+    '@twofingers': function(e) {
+      // no-op
+    },
+    '@redispatch': function() {
+      this.runState('@waiting');
+    },
+    '@redispatch:click': function(e) {
+      this.removeListeners();
+      fireOriginalEvent(this.refEvent);
+      this.setListeners();
+      this.emit('tap', e);
+      this.runState('@cooldown');
+    },
+    '@pinchstart': function(e) {
+      this.emit('pinchstart', e);
+      this.runState('@pinch', e);
+    },
+    '@pinch': function(e) {
+      // pending
+    },
+    '@cooldown': function() {
+      // no-op
+    },
+    '@taptwo': function(e) {
+      this.emit('taptwo', e);
+      this.runState('@waiting');
+    }
+  },
+  transitions: {
+    '@waiting': {
+      'FIRST_FINGER_DOWN': '@hold',
+      'SECOND_FINGER_DOWN': '@twofingers'
+    },
+    '@hold': {
+      'SECOND_FINGER_DOWN': '@twofingers',
+      // 'TIME_OUT': '@redispatch',
+      'TOUCH_MOVE': '@dragstart',
+      'TOUCH_END_FIRST_FINGER': '@redispatch:click'
+    },
+    '@drag': {
+      'SECOND_FINGER_DOWN': '@cooldown',
+      'TOUCH_END_FIRST_FINGER': '@swipe',
+      'TOUCH_MOVE': '@drag'
+    },
+    '@twofingers': {
+      'TOUCH_MOVE': '@pinchstart',
+      'TOUCH_END_SECOND_FINGER': '@ready',
+      'TOUCH_END_BOTH_FINGERS': '@taptwo',
+      'TIME_OUT': '@waiting'
+    },
+    '@dragend': {
+      'TIME_OUT': '@waiting'
+    },
+    '@pinch': {
+      'TOUCH_MOVE': '@pinch',
+      'TOUCH_END_FIRST_FINGER': '@waiting',
+      'TOUCH_END_SECOND_FINGER': '@waiting',
+      'TOUCH_END_BOTH_FINGERS': '@waiting'
+    },
+    '@ready': {
+      'TIME_OUT': '@redispatch',
+      'TOUCH_END_FIRST_FINGER': '@taptwo'
+    },
+    '@cooldown': {
+      'TIME_OUT': '@waiting'
+    }
+  }
+}, configConstants);
+
+// Let's GO!
+
+if (typeof module !== 'undefined')
+  module.exports = TouchDaemon.start();
